fc42a08b28d59c5c0718fe56a8c3eb765c5bacba
Allison MacQueen <allimacq@gmail.com>
2020-11-05 17:41:47 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,7 +12,7 @@ class Backer
     @project = project
     @backed_projects << @project
     new_backer = Backer.new(@name)
-   p  @project.add_backer(new_backer)
+   @project.add_backer(new_backer)
   end
 
   #describe 'Backer - More Advanced #back_project' do
